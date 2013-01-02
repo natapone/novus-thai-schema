243929769e28121730c76beaa6340237291c61d8
Dong Charsombut <natapone@gmail.com>
2013-01-02 07:57:40 +0700

    add table ngram , ItemGram

--- lib/novus/thai/schema/Result/Ngram.pm
+++ lib/novus/thai/schema/Result/Ngram.pm
@@ -1,12 +1,12 @@
 use utf8;
-package novus::thai::schema::Result::ItemNgram;
+package novus::thai::schema::Result::Ngram;
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
 
 =head1 NAME
 
-novus::thai::schema::Result::ItemNgram
+novus::thai::schema::Result::Ngram
 
 =cut
 
@@ -27,20 +27,15 @@ use base 'DBIx::Class::Core';
 
 __PACKAGE__->load_components("InflateColumn::DateTime");
 
-=head1 TABLE: C<item_ngram>
+=head1 TABLE: C<ngram>
 
 =cut
 
-__PACKAGE__->table("item_ngram");
+__PACKAGE__->table("ngram");
 
 =head1 ACCESSORS
 
-=head2 itemid
-
-  data_type: 'bigint'
-  is_nullable: 0
-
-=head2 ngram_by_id
+=head2 ngramid
 
   data_type: 'varchar'
   is_nullable: 0
@@ -52,44 +47,59 @@ __PACKAGE__->table("item_ngram");
   is_nullable: 1
   size: 1024
 
-=head2 ngram_length
+=head2 document_frequency
 
   data_type: 'integer'
+  is_nullable: 1
+
+=head2 ngram_length
+
+  data_type: 'smallint'
   default_value: 1
   is_nullable: 1
 
 =cut
 
 __PACKAGE__->add_columns(
-  "itemid",
-  { data_type => "bigint", is_nullable => 0 },
-  "ngram_by_id",
+  "ngramid",
   { data_type => "varchar", is_nullable => 0, size => 1024 },
   "term",
   { data_type => "varchar", is_nullable => 1, size => 1024 },
-  "ngram_length",
-  { data_type => "smallint", is_nullable => 1 },
-  "ngram_count",
+  "document_frequency",
   { data_type => "integer", is_nullable => 1 },
+  "ngram_length",
+  { data_type => "smallint", default_value => 1, is_nullable => 1 },
 );
 
 =head1 PRIMARY KEY
 
 =over 4
 
-=item * L</itemid>
-
-=item * L</ngram_by_id>
+=item * L</ngramid>
 
 =back
 
 =cut
 
-__PACKAGE__->set_primary_key("itemid", "ngram_by_id");
+__PACKAGE__->set_primary_key("ngramid");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<ngram_term_key>
+
+=over 4
+
+=item * L</term>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("ngram_term_key", ["term"]);
 
 
-# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-12-28 11:48:34
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7JgudNOO4UlhKAX6V2ukjQ
+# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-12-30 22:28:34
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:753rNCCpX1DgF7BeTq0a9g
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
